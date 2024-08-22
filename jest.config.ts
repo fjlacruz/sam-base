38066ea5381ba26daadb30e1e398dc8bd6136800
Javier La Cruz <idsistemas15@gmail.com>
2024-08-22 09:11:30 -0400

    4

--- jest.config.ts
+++ jest.config.ts
@@ -14,7 +14,7 @@ export default {
     testMatch: ['**/tests/unit/*.test.ts'],
     coverageThreshold: {
         global: {
-          branches: 80,  // Porcentaje mínimo de cobertura de branches
+          branches: 70,  // Porcentaje mínimo de cobertura de branches
           functions: 70, // Porcentaje mínimo de cobertura de funciones
           lines: 70,     // Porcentaje mínimo de cobertura de líneas
           statements: 70 // Porcentaje mínimo de cobertura de statements
