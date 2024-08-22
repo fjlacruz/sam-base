e6770d9c7ed0984a707de0184047c28ad3bd1daf
Javier La Cruz <idsistemas15@gmail.com>
2024-08-22 15:01:54 -0400

    stage

--- src/lambdas/app.ts
+++ src/lambdas/app.ts
@@ -17,7 +17,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
             statusCode: 200,
             body: JSON.stringify({
                 message: 'sam base',
-                table
+                stage
             }),
         };
     } catch (err) {
